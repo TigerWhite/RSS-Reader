35fff07383e8355cf66abc426b5371a38502f88b
hieund <hieund@NGUYENDUCHIEU>
2014-07-16 16:54:28 +0700

    chia theo chu de

--- RssReaderTest/src/com/example/rssreadertest/MainActivity.java
+++ RssReaderTest/src/com/example/rssreadertest/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.StrictMode;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,20 +22,23 @@ import android.widget.Toast;
 
 import com.ltt.rssreader.RssItemInfo;
 import com.ltt.rssreader.RssParser;
+import com.ltt.util.SourceHelper;
 import com.ltt.util.WebAccessHandler;
 
 public class MainActivity extends Activity {
 
-	private Spinner spinner;
+	private Spinner spinner1;
+	private Spinner spinner2;
 	private Button btnOk;
 	private ListView listViewItem;
 	private ArrayList<RssItemInfo> listData;
+	private SourceHelper sh;
 	private OnClickListener btnGetImgListener = new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
 			WebAccessHandler webhandle = new WebAccessHandler();
-			String link = spinner.getSelectedItem().toString();
+			String link = spinner2.getSelectedItem().toString();
 			
 			// Thuc hien phan tich XML
 			InputStream inStream = null;
@@ -96,7 +100,6 @@ public class MainActivity extends Activity {
 	            });
 		}
 	};
-
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,18 +109,60 @@ public class MainActivity extends Activity {
         StrictMode.setThreadPolicy(policy);
 
         btnOk = (Button)findViewById(R.id.btnOk);
-        spinner = (Spinner)findViewById(R.id.spinner1);
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+		btnOk.setOnClickListener(btnGetImgListener);
+		sh = new SourceHelper(this);
+
+		// Create an ArrayAdapter using a default spinner1 layout
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item);
+		final ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item);
+
+		// Set content to the adapter
+		for (String item : sh.getCategories()) {
+			adapter.add(item);
+		}
+
+		for (String item : sh.getItems()) {
+			adapter2.add(item);
+		}
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		// Apply the adapter to the spinner1
+		spinner1.setAdapter(adapter);
+		spinner2.setAdapter(adapter2);
+
+		spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				String itemName = (String) parent.getItemAtPosition(pos);
+
+				sh.setItemsFromCategory(itemName);
+				// Set content to the adapter
+				adapter2.clear();
+				for (String item : sh.getItems()) {
+					adapter2.add(item);
+				}
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				Toast.makeText(getApplicationContext(), "nothing selected",
+						Toast.LENGTH_LONG).show();
+
+			}
+		});
+
 		listViewItem=(ListView)findViewById(R.id.listView1);
 
         btnOk.setOnClickListener(btnGetImgListener);
-        
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.arrayUrl, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
     }
 
 }
--- XMLParcer/src/com/example/xmlparser/MainActivity.java
+++ XMLParcer/src/com/example/xmlparser/MainActivity.java
@@ -15,6 +15,8 @@ import android.os.StrictMode;
 import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -24,6 +26,7 @@ import android.widget.Toast;
 
 import com.ltt.rssreader.RssItemInfo;
 import com.ltt.rssreader.RssParser;
+import com.ltt.util.SourceHelper;
 import com.ltt.util.WebAccessHandler;
 
 public class MainActivity extends Activity {
@@ -31,9 +34,11 @@ public class MainActivity extends Activity {
 	private ArrayList<RssItemInfo> listData;
 	private LinearLayout ll;
 	private TextView tv;
-//	private WebView wv;
+	// private WebView wv;
 	private Button btnOk;
-	private Spinner spinner;
+	private Spinner spinner1;
+	private Spinner spinner2;
+	private SourceHelper sh;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -45,20 +50,58 @@ public class MainActivity extends Activity {
 
 		ll = (LinearLayout) findViewById(R.id.linearLayout1);
 		tv = new TextView(this);
-		
+
 		btnOk = (Button) findViewById(R.id.btnOk);
-		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		spinner2 = (Spinner) findViewById(R.id.spinner2);
 
 		btnOk.setOnClickListener(btnGetImgListener);
+		sh = new SourceHelper(this);
 
-		// Create an ArrayAdapter using the string array and a default spinner
-		// layout
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-				this, R.array.arrayUrl, android.R.layout.simple_spinner_item);
+		// Create an ArrayAdapter using a default spinner1 layout
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item);
+		final ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item);
+
+		// Set content to the adapter
+		for (String item : sh.getCategories()) {
+			adapter.add(item);
+		}
+
+		for (String item : sh.getItems()) {
+			adapter2.add(item);
+		}
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
+		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		// Apply the adapter to the spinner1
+		spinner1.setAdapter(adapter);
+		spinner2.setAdapter(adapter2);
+
+		spinner1.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				String itemName = (String) parent.getItemAtPosition(pos);
+
+				sh.setItemsFromCategory(itemName);
+				// Set content to the adapter
+				adapter2.clear();
+				for (String item : sh.getItems()) {
+					adapter2.add(item);
+				}
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				Toast.makeText(getApplicationContext(), "nothing selected",
+						Toast.LENGTH_LONG).show();
+
+			}
+		});
 
 	}
 
@@ -67,11 +110,11 @@ public class MainActivity extends Activity {
 		@Override
 		public void onClick(View v) {
 			WebAccessHandler webhandle = new WebAccessHandler();
-			String link = spinner.getSelectedItem().toString();
+			String link = spinner1.getSelectedItem().toString();
 
 			// Thuc hien phan tich XML
 			InputStream inStream = null;
-			
+
 			try {
 				inStream = webhandle.getStreamFromUrl(link);
 			} catch (ClientProtocolException e1) {
@@ -79,15 +122,15 @@ public class MainActivity extends Activity {
 				Toast.makeText(MainActivity.this, "loi phuong thuc",
 						Toast.LENGTH_SHORT).show();
 				return;
-	
+
 			} catch (IOException e1) {
 				e1.printStackTrace();
 				Toast.makeText(MainActivity.this, "loi ket noi",
 						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			
-			if (inStream == null){
+
+			if (inStream == null) {
 				try {
 					inStream = webhandle.fetchURL(link);
 				} catch (MalformedURLException e) {
@@ -102,54 +145,57 @@ public class MainActivity extends Activity {
 					return;
 				}
 			}
-			
+
 			listData = new RssParser().parseXML(inStream);
 			ll.removeAllViews();
 			try {
 				printData(listData);
-//				printContent(inStream);
+				// printContent(inStream);
 			} catch (NullPointerException e) {
 				e.printStackTrace();
-				Toast.makeText(MainActivity.this, "no data",
-						Toast.LENGTH_SHORT).show();
+				Toast.makeText(MainActivity.this, "no data", Toast.LENGTH_SHORT)
+						.show();
 				return;
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
+				// } catch (IOException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
 			}
 
 		}
 	};
-	
+
 	// in kq tu list lay duoc
-	private void printData(ArrayList<RssItemInfo> cartList) throws NullPointerException{
+	private void printData(ArrayList<RssItemInfo> cartList)
+			throws NullPointerException {
 
 		for (RssItemInfo RssItemInfo : cartList) {
 			tv = new TextView(this);
 			tv.setText("TIEUDE : " + RssItemInfo.getTitle());
 			ll.addView(tv);
-			
+
 			tv = new TextView(this);
 			tv.setText("MOTA : " + Html.fromHtml(RssItemInfo.getDescription()));
 			ll.addView(tv);
-//			wv = new WebView(this);
-//			wv.setLayoutParams(new WebView.LayoutParams(LayoutParams.WRAP_CONTENT, 
-//                    LayoutParams.WRAP_CONTENT, 0, 0));
-//			wv.loadDataWithBaseURL(RssItemInfo.getQuantity(), RssItemInfo.getItemNumber(), "text/html", "utf-8", null);
-//			ll.addView(wv);
-			
+			// wv = new WebView(this);
+			// wv.setLayoutParams(new
+			// WebView.LayoutParams(LayoutParams.WRAP_CONTENT,
+			// LayoutParams.WRAP_CONTENT, 0, 0));
+			// wv.loadDataWithBaseURL(RssItemInfo.getQuantity(),
+			// RssItemInfo.getItemNumber(), "text/html", "utf-8", null);
+			// ll.addView(wv);
+
 			tv = new TextView(this);
 			tv.setText("LINK : " + RssItemInfo.getLink());
 			ll.addView(tv);
-			
+
 			tv = new TextView(this);
 			tv.setText("THUMB : " + RssItemInfo.getThumbnail());
 			ll.addView(tv);
-			
+
 			tv = new TextView(this);
 			tv.setText("DATE : " + RssItemInfo.getPubDate());
 			ll.addView(tv);
-			
+
 			tv = new TextView(this);
 			tv.setText("---");
 			ll.addView(tv);
