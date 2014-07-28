6a1c9cc59cd41b079d03af88870c3e794c214987
zendbui <endlovez@gmail.com>
2014-07-28 14:27:01 +0700

    build project's skeleton

--- RSS Reader/src/ltt/intership/activity/StartUpActivity.java
+++ RSS Reader/src/ltt/intership/activity/StartUpActivity.java
@@ -1,15 +1,10 @@
-package ltt.intership;
+package ltt.intership.activity;
 
+import ltt.intership.R;
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class StartUpActivity extends Activity {
 
@@ -17,11 +12,6 @@ public class StartUpActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start_up);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
@@ -42,23 +32,6 @@ public class StartUpActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_start_up,
-					container, false);
-			return rootView;
-		}
 	}
 
 }
