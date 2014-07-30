b39d0e7a20d8457abfc4ecb26418f5ea2018b8bf
zendbui <endlovez@gmail.com>
2014-07-30 16:50:18 +0700

    chinh setting library FacebookSDK va project LoginTimeLine

--- LoginTimeLine/src/com/facebook/scrumptious/SelectionFragment.java
+++ LoginTimeLine/src/com/facebook/scrumptious/SelectionFragment.java
@@ -2,23 +2,19 @@ package com.facebook.scrumptious;
 
 import org.json.JSONObject;
 
-
-import com.facebook.HttpMethod;
-import com.facebook.Request;
-import com.facebook.RequestAsyncTask;
-import com.facebook.Response;
-import com.facebook.Session;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
 import android.widget.Button;
-import android.widget.TextView;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
+import com.facebook.Session;
 
 public class SelectionFragment extends Fragment {
 	private static final String TAG = "SelectionFragment";
--- RSS Reader Lib/src/com/ltt/rssreader/RssParser.java
+++ RSS Reader Lib/src/com/ltt/rssreader/RssParser.java
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-package com.ltt.rssreader;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
-import android.util.Log;
-
-public class RssParser {
-
-	/**
-	 * Hàm phân tích XML
-	 * 
-	 * @param is
-	 *            InputStream
-	 * @return list of RSSItemInfo
-	 */
-	public ArrayList<RssItemInfo> parseXML(InputStream is) {
-		ArrayList<RssItemInfo> cartList = null;
-		try {
-			// Tạo đối tượng dùng cho việc phân tích cú pháp tài liệu XML
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp = spf.newSAXParser();
-			// Đối tượng đọc XML
-			XMLReader xr = sp.getXMLReader();
-
-			// Tạo đối tượng xử lý XML theo tuần tự của mình
-			RssXMLHandler myXMLHandler = new RssXMLHandler();
-			// Thiết lập nội dung xử lý
-			xr.setContentHandler(myXMLHandler);
-			// Nguồn dữ liệu vào
-			InputSource inStream = new InputSource(is);
-			// Bắt đầu xử lý dữ liệu vào
-			xr.parse(inStream);
-
-			// In chi tiết sản phẩm ra giao diện ứng dụng
-			cartList = myXMLHandler.getCartList();
-
-			is.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.e("xml reader", "loi format xml");
-		}
-
-		return cartList;
-	}
-
-}
-=======
 package com.ltt.rssreader;
 
 import java.io.InputStream;
@@ -111,4 +56,3 @@ public class RssParser {
 	}
 
 }
->>>>>>> staging
